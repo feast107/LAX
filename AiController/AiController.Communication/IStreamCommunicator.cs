275c9c597a421261064c4e82b8c41a27ddaf1b5d
Feast <1353216996@qq.com>
2023-03-26 18:59:26 +0800

    move

---
+++ AiController/AiController.Communication/IStreamCommunicator.cs
@@ -0,0 +1,8 @@
+﻿namespace AiController.Communication
+{
+    public interface IStreamCommunicator
+    {
+        delegate void MessageHandler(string message);
+        Task Send(string message, MessageHandler handler, CancellationToken token = default);
+    }
+}
