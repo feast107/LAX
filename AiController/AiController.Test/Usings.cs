e6a3dd092544beb481b0bc3335486bd4880fb739
Feast <1353216996@qq.com>
2023-03-26 18:55:49 +0800

    move

---
+++ AiController/AiController.Test/Usings.cs
@@ -0,0 +1,5 @@
+global using NUnit.Framework;
+global using AiController.Communication;
+global using AiController.Conversion;
+global using AiController.Server;
+global using AiController.Transmission;
