dea061dc4a5e70ebd0e0b74f0ab1cbb10721547f
Feast <1353216996@qq.com>
2023-03-28 00:10:32 +0800

    Move

---
+++ AiController/AiController.Abstraction/Communication/IAsyncCommunicator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AiController.Abstraction.Communication
+{
+    public interface IAsyncCommunicator
+    {
+        Task<string> SendAsync(string message, CancellationToken token = default);
+    }
+}
--- AiController/AiController.Communication/IAsyncCommunicator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AiController.Communication
-{
-    public interface IAsyncCommunicator
-    {
-        Task<string> SendAsync(string message,CancellationToken token = default);
-    }
-}
