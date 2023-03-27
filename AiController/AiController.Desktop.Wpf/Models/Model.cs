35ed7b46c94d5f97b8910146507ee0e689c85601
Feast <1353216996@qq.com>
2023-03-28 00:11:42 +0800

    Create Model.cs

---
+++ AiController/AiController.Desktop.Wpf/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiController.Desktop.Wpf.Models
+{
+    public partial class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
