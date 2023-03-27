55308f37b6c5fb216c3941e754d0cdcfd410a5d3
Feast <1353216996@qq.com>
2023-03-28 00:59:35 +0800

    Create CommandTest.cs

---
+++ AiController/AiController.Test/CommandTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiController.Test
+{
+    internal class CommandTest
+    {
+        [Test]
+        public void Test()
+        {
+            Command command = new Command("app -myoption 123");
+            Assert.Pass(command.Arguments[0].ToString());
+        }
+    }
+}
