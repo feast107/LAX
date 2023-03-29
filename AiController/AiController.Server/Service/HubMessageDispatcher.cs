e0631a478081b8da515eb5c6ff704fb293008bff
Feast <feast107@gmail.com>
2023-03-29 21:34:39 +0800

    Create HubMessageDispatcher.cs

---
+++ AiController/AiController.Server/Service/HubMessageDispatcher.cs
@@ -0,0 +1,47 @@
+﻿using AiController.Abstraction.Communication;
+using AiController.Abstraction.Conversion;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AiController.Server.Service
+{
+    public class HubMessageDispatcher<THub> : IHubDispatchService<THub> where THub : Hub
+    {
+        public HubMessageDispatcher(IAsyncCommunicator communicator,
+            IOperationConverter<Action<Dictionary<string, THub>>> operationConverter)
+        {
+            this.communicator = communicator;
+            this.operationConverter = operationConverter;
+        }
+
+        private readonly Dictionary<string, THub> ConnectedHubs = new();
+
+        private readonly IAsyncCommunicator communicator;
+
+        private readonly IOperationConverter<Action<Dictionary<string, THub>>> operationConverter;
+
+        public void OnHubDisconnect(THub hub)
+        {
+            foreach(var pair in ConnectedHubs)
+            {
+                if(pair.Value == hub)
+                {
+                    ConnectedHubs.Remove(pair.Key);
+                }
+            }
+        }
+
+        public void OnReceiveMessage(THub hub, string message)
+        {
+            communicator
+                .SendAsync(operationConverter
+                .ToMessage(message))
+                .ContinueWith(x => {
+                    operationConverter
+                    .ToOperation(x.Result)
+                    .Invoke(ConnectedHubs);
+                });
+        }
+
+        public bool OnRegister(THub hub, string name) => ConnectedHubs.TryAdd(name, hub);
+    }
+}
