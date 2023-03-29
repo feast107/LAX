820e8938ebe62529b80edb90b523d9a51339b892
Feast <feast107@gmail.com>
2023-03-29 21:35:00 +0800

    Update UnitTest1.cs

--- AiController/AiController.Test/UnitTest1.cs
+++ AiController/AiController.Test/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace AiController.Test
     public class Tests
     {
         private IAsyncCommunicator Communicator { get; set; }
-        [SetUp]
+        [SetUp()]
         public void Setup()
         {
             Communicator = new Gpt35AsyncCommunicator()
@@ -18,14 +18,13 @@ namespace AiController.Test
             };
         }
 
-      
-
         [Test]
         public async Task Test1()
         {
             try
             {
-                var res = await Communicator.SendAsync("你能做到什么");
+                var res = await Communicator.SendAsync("" +
+                    "针对这段要求的回复，请转换成简短的命令行：在 d:MyDir 目录下创建一个名为Access的目录");
                 Assert.IsNotNull(res);
             }
             catch (Exception e)
