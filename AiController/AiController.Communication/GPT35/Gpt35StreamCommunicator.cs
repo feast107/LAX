77a580aa37d651f44935cf91257336e2c5ca6f4c
Feast <feast107@gmail.com>
2023-03-30 00:11:04 +0800

    Update Gpt35StreamCommunicator.cs

--- AiController/AiController.Communication/GPT35/Gpt35StreamCommunicator.cs
+++ AiController/AiController.Communication/GPT35/Gpt35StreamCommunicator.cs
@@ -4,22 +4,19 @@ using OpenAI.Chat;
 
 namespace AiController.Communication.GPT35
 {
-    public class Gpt35StreamCommunicator : GptBaseCommunicator , IStreamCommunicator
+    public class Gpt35StreamCommunicator : GptBaseCommunicator, IStreamCommunicator
     {
         public Task Send(string message,
             IStreamCommunicator.MessageHandler handler,
             CancellationToken token = default)
         {
             return Client.ChatEndpoint.StreamCompletionAsync(
-                new(
-                    new[] { new ChatPrompt("user", message) },
-                    ModelName,
-                    Temperature), response =>
+               GetChatRequest(new ChatPrompt(nameof(Role.user), message)), response =>
                 {
                     if (response == null || token.IsCancellationRequested) return;
                     handler?
                         .Invoke(response.Choices
-                        .Aggregate(new StringBuilder(), 
+                        .Aggregate(new StringBuilder(),
                             (sb, c) => sb.AppendLine(c.Delta.Content))
                         .ToString());
                 }, token);
