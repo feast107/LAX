bfe2747023a1f33a41222bf89c5b08da831bceed
Feast <feast107@gmail.com>
2023-03-29 21:34:35 +0800

    Create MessageHub.cs

---
+++ AiController/AiController.Server/Hubs/MessageHub.cs
@@ -0,0 +1,34 @@
+﻿using AiController.Abstraction.Communication;
+using AiController.Communication.GPT35;
+using AiController.Server.Service;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AiController.Server.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public MessageHub(IHubDispatchService<MessageHub> service)
+        {
+            Service = service;
+        }
+        public readonly IHubDispatchService<MessageHub> Service;
+
+        public override Task OnConnectedAsync()
+        {
+            Service.OnHubConnect(this);
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            Service.OnHubDisconnect(this);
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task Register(string name) =>
+            await Clients
+            .Caller
+            .SendAsync(nameof(Register), 
+                Service.OnRegister(this, name));
+
+        public void SendMessage(string message) => Service.OnReceiveMessage(this, message);
+    }
+}
