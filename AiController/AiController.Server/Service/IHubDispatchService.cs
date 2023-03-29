ce8c03f2d71a7ffa3e075be305244d795cf5039d
Feast <feast107@gmail.com>
2023-03-29 21:34:41 +0800

    Create IHubDispatchService.cs

---
+++ AiController/AiController.Server/Service/IHubDispatchService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AiController.Server.Service
+{
+    public interface IHubDispatchService<THub> where THub : Hub
+    {
+        void OnHubDisconnect(THub hub);
+        bool OnRegister(THub hub, string name);
+        void OnReceiveMessage(THub hub, string message);
+    }
+}
