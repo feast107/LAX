b2be19505c2de226551e2a25ad78be1d7d8e949b
Feast <feast107@gmail.com>
2023-03-31 00:30:45 +0800

    Update HubMessageDispatcher.cs

--- AiController/AiController.Server/Service/HubMessageDispatcher.cs
+++ AiController/AiController.Server/Service/HubMessageDispatcher.cs
@@ -1,6 +1,7 @@
 ﻿using AiController.Abstraction;
+using AiController.Infrastructure;
 using AiController.Operation.Operators.Direct;
-using AiController.Server.Hubs;
+using AiController.Transmission.SignalR;
 using Microsoft.AspNetCore.SignalR;
 
 namespace AiController.Server.Service
@@ -12,38 +13,48 @@ namespace AiController.Server.Service
             this.asyncOperator = asyncOperator;
         }
 
-        private readonly Dictionary<string, Tuple<IDescriptor, THub>> ConnectedHubs = new();
+        private readonly Dictionary<string, Tuple<IDescriptor, THub>> connectedHubs = new();
 
         private readonly Gpt35DistributeAsyncOperator asyncOperator;
 
-        private Task<Tuple<string, string>?>? CurrentRequest;
+        private Task<DistributeMessageModel?>? currentRequest;
 
         public void OnHubDisconnect(string connectionId)
         {
-            ConnectedHubs.Remove(connectionId);
+            if (!connectedHubs.TryGetValue(connectionId, out var field)) return;
+            asyncOperator.Remove(field.Item1);
+            connectedHubs.Remove(connectionId);
         }
 
-        public void OnReceiveMessage(THub hub, string message)
+        public async Task OnReceiveMessage(THub hub, string message)
         {
-            if (CurrentRequest == null)
+            var field = connectedHubs[hub.Context.ConnectionId];
+            currentRequest = currentRequest == null
+                ? asyncOperator.SendAsync($"{field.Item1.Identifier} \n{message}")
+                : currentRequest.ContinueWith(_ => asyncOperator.SendAsync(message).Result);
+            await currentRequest.ContinueWith(task =>
             {
-                CurrentRequest = asyncOperator.SendAsync(message);
-            }
-            else
-            {
-                CurrentRequest = CurrentRequest.ContinueWith(t => asyncOperator.SendAsync(message).Result);
-            }
-            CurrentRequest.ContinueWith(task =>
+                if (task.Result?.device == null) return;
+                Console.WriteLine(task.Result);
+                foreach (var pair in connectedHubs)
                 {
-                    if (task.Result == null) return;
-                    if (ConnectedHubs.TryGetValue(task.Result.Item1, out var tuple))
-                    {
-                        tuple.Item2.Clients.Caller.SendAsync(task.Result.Item2);
-                    }
-                });
+                    if (pair.Value.Item1.Identifier.Trim() != task.Result.device.Trim()) return;
+                    hub.Clients
+                        .Client(pair.Key)
+                        .SendAsync(nameof(InvokeMethod.Receive), task.Result.reply);
+                }
+            });
         }
 
-        public bool OnRegister(THub hub, IDescriptor identifier) =>
-            ConnectedHubs.TryAdd(hub.Context.ConnectionId, new(identifier, hub));
+        public bool OnRegister(THub hub, IDescriptor identifier)
+        {
+            if (connectedHubs.TryGetValue(hub.Context.ConnectionId, out var field))
+            {
+                asyncOperator.Remove(field.Item1);
+            }
+            connectedHubs[hub.Context.ConnectionId] = new(identifier, hub);
+            asyncOperator.Add(identifier);
+            return true.With(connectedHubs.Count);
+        }
     }
 }
