c495d73daf51552194e808eb09c39bf90a850b76
Feast <feast107@gmail.com>
2023-03-30 09:43:52 +0800

    Create IAsyncOperator.cs

---
+++ AiController/AiController.Abstraction/Operation/IAsyncOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiController.Abstraction.Operation
+{
+    internal interface IAsyncOperator<TOperation> : IDescriptor
+    {
+        Task<TOperation> SendAsync(object ask);
+    }
+}
