4af152d27747281f8b4df2279ce06ff981f671b2
Feast <feast107@gmail.com>
2023-03-30 09:43:47 +0800

    Change

---
+++ AiController/AiController.Abstraction/Operation/IOperator.cs
@@ -0,0 +1,9 @@
+﻿namespace AiController.Abstraction.Conversion
+{
+    public interface IOperationConverter : IOperator<object> { }
+
+    public interface IOperator<out TOperation> : IDescriptor
+    {
+        TOperation Send(object ask);
+    }
+}
