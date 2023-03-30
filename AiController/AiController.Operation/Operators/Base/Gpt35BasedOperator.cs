8b65abdd7ff30b6204c328d888cdc1c97bacf479
Feast <feast107@gmail.com>
2023-03-31 00:30:27 +0800

    Update Gpt35BasedOperator.cs

--- AiController/AiController.Operation/Operators/Base/Gpt35BasedOperator.cs
+++ AiController/AiController.Operation/Operators/Base/Gpt35BasedOperator.cs
@@ -2,17 +2,15 @@
 using AiController.Abstraction.Communication;
 using AiController.Communication.GPT35;
 using OpenAI.Chat;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using AiController.Infrastructure;
 
 namespace AiController.Operation.Operators.Base;
 
 public abstract class Gpt35BasedOperator : IDescriptor 
 {
-    public string Description { get; set; } = string.Empty;
-    public required string Identifier { get; init; }
-
-    public readonly List<IDescriptor> Clients = new ();
+    public abstract string Description { get; set; }
+    public required string Identifier { get; init; } = "这是中心服务器:[Server]";
 
     protected Gpt35BasedOperator(IAsyncCommunicator<ChatPrompt[]> communicator)
     {
@@ -23,10 +21,11 @@ public abstract class Gpt35BasedOperator : IDescriptor
     protected virtual Task<string> SendAsyncInternal(string ask)
     {
         return Communicator.SendAsync(
-            new ChatPrompt[] {
+            new [] {
                 Identifier.ToChatPrompt(Role.system),
                 Description.ToChatPrompt(Role.system),
-                ask.ToChatPrompt(Role.user)
+                ask.ToChatPrompt()
             });
     }
 }
+
