81c453f22baf0dea794c9fb53e01c8f2dd6ea4a3
Feast <feast107@gmail.com>
2023-03-31 00:30:49 +0800

    Create DistributeMessageModel.cs

---
+++ AiController/AiController.Transmission/SignalR/DistributeMessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace AiController.Transmission.SignalR
+{
+    public class DistributeMessageModel
+    {
+        public string? device { get; set; }
+        public string? reply { get; set; }
+
+        public static string Example { get; } = JsonSerializer.Serialize(new DistributeMessageModel()
+        {
+            device = "deviceId",
+            reply = "this is the reply of message"
+        });
+    }
+}
