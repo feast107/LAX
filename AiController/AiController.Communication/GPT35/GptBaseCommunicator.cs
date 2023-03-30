d86f6853bd82799a973504ac29fd07b720e8fd68
Feast <feast107@gmail.com>
2023-03-31 00:30:16 +0800

    Update GptBaseCommunicator.cs

--- AiController/AiController.Communication/GPT35/GptBaseCommunicator.cs
+++ AiController/AiController.Communication/GPT35/GptBaseCommunicator.cs
@@ -26,6 +26,6 @@ namespace AiController.Communication.GPT35
 
     public static class ChatGptExtension 
     {
-        public static ChatPrompt ToChatPrompt(this string message, Role role) => new (role.ToString(), message);
+        public static ChatPrompt ToChatPrompt(this string message, Role role = Role.user) => new (role.ToString(), message);
     }
 }
