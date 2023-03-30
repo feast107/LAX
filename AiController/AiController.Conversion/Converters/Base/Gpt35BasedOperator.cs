5e6e916b0bb7bca37e25f5e0256635d892f506f3
Feast <feast107@gmail.com>
2023-03-30 09:43:56 +0800

    Create Gpt35BasedOperator.cs

---
+++ AiController/AiController.Conversion/Converters/Base/Gpt35BasedOperator.cs
@@ -0,0 +1,22 @@
+﻿using AiController.Abstraction.Conversion;
+using System;
+
+namespace AiController.Conversion.Converters.Base
+{
+    public abstract class Gpt35BasedOperator<TOperation> : IOperator<TOperation>
+    {
+        public object Context { get; set; }
+
+        public string ToMessage(object ask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToMessage(object subject, object ask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract TOperation ToOperation(string reply);
+    }
+}
