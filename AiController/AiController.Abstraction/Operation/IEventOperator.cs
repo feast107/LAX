ed3b766148594763b6590273a94950c6063dbe2f
Feast <feast107@gmail.com>
2023-03-30 09:43:54 +0800

    Create IEventOperator.cs

---
+++ AiController/AiController.Abstraction/Operation/IEventOperator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiController.Abstraction.Operation
+{
+    internal interface IEventOperator<TOperation>
+    {
+        void Send(string ask);
+
+        delegate void OperationHandler(TOperation operation);
+        OperationHandler OnReceiveOperation { get; set; } 
+    }
+}
