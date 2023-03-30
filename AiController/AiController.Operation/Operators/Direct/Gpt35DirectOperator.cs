7842884ba6cf147a3d9c0a91c6257e5531e61921
Feast <feast107@gmail.com>
2023-03-31 00:30:31 +0800

    Update Gpt35DirectOperator.cs

--- AiController/AiController.Operation/Operators/Direct/Gpt35DirectOperator.cs
+++ AiController/AiController.Operation/Operators/Direct/Gpt35DirectOperator.cs
@@ -11,6 +11,7 @@ namespace AiController.Operation.Operators.Direct
         { }
 
         public IEventOperator<string>.OperationHandler? OnReceiveOperation { get; set; }
+        public override string Description { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
         public void Send(string ask)
         {
