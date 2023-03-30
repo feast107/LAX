9f0d15cf8988beb7404ba6737027dfbeee7da72f
Feast <feast107@gmail.com>
2023-03-31 00:30:41 +0800

    Update MessageHub.cs

--- AiController/AiController.Server/Hubs/MessageHub.cs
+++ AiController/AiController.Server/Hubs/MessageHub.cs
@@ -16,17 +16,19 @@ namespace AiController.Server.Hubs
         {
             return base.OnConnectedAsync();
         }
+
         public override Task OnDisconnectedAsync(Exception? exception)
         {
             Service.OnHubDisconnect(Context.ConnectionId);
             return base.OnDisconnectedAsync(exception);
         }
+
         public async Task Register(MessageModel message) =>
             await Clients
             .Caller
             .SendAsync(nameof(Register), 
                 Service.OnRegister(this, message));
 
-        public void SendMessage(string message) => Service.OnReceiveMessage(this, message);
+        public async Task Send(string message) => await Service.OnReceiveMessage(this, message);
     }
 }
