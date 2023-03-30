209f036797e97031297951e7b6d03ef5717da3b3
Feast <feast107@gmail.com>
2023-03-30 09:44:07 +0800

    Create RequiredAttribute.cs

---
+++ AiController/AiController.Abstraction/External/RequiredAttribute.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    internal sealed class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public const string RefStructs = "RefStructs";
+
+        public const string RequiredMembers = "RequiredMembers";
+
+        public string FeatureName { get; }
+
+        public bool IsOptional { get; init; }
+
+        public CompilerFeatureRequiredAttribute(string featureName)
+        {
+            FeatureName = featureName;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    internal sealed class RequiredMemberAttribute : Attribute { }
+    internal static class IsExternalInit { }
+}
+
