577132e7a332915fd4ce6fa3a82cd2bfe2f138b9
Feast <feast107@gmail.com>
2023-03-31 00:30:24 +0800

    Create DebugExtension.cs

---
+++ AiController/AiController.Infrastructure/DebugExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiController.Infrastructure
+{
+    public static class DebugExtension
+    {
+        public static T With<T>(this T then, params object[] args)
+        {
+            args.ForEach(Console.WriteLine);
+            return then;
+        }
+
+        public static string Ease(this string json) => json.Trim().Length == 0 ? "{}" : json;
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
