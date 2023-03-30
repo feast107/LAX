154531d83b175fb1a3812b7d18efe3f83fc25b1f
Feast <feast107@gmail.com>
2023-03-31 00:30:29 +0800

    Create JsonBasedOperation.cs

---
+++ AiController/AiController.Operation/Operators/Base/JsonBasedOperation.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace AiController.Operation.Operators.Base
+{
+    public static class JsonBasedOperation
+    {
+        public static JsonSerializerOptions Options { get; } = new ()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        public static string Serialize<TModel>(this TModel model) => JsonSerializer.Serialize<TModel>(model, Options);
+        public static TModel? Deserialize<TModel>(this string reply) => JsonSerializer.Deserialize<TModel>(reply, Options);
+    }
+}
