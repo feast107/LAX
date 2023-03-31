5c551920dd64a2a8bb517217825df996c3371c77
Feast <1353216996@qq.com>
2023-03-31 11:26:02 +0800

    Create IProxied.cs

---
+++ AiController/AiController.Abstraction/Operation/IProxied.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiController.Abstraction.Operation
+{
+    public interface IProxied<TProxy>
+    {
+        TProxy Proxy { get; set; }
+    }
+}
