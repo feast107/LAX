1bb6f8c3cc506081e1f020e3607be11b4e27bdf3
Feast <1353216996@qq.com>
2023-03-31 11:11:02 +0800

    Create Gpt35ClientOperator.cs

---
+++ AiController/AiController.Operation/Operators/Indirect/Gpt35ClientOperator.cs
@@ -0,0 +1,20 @@
+﻿using AiController.Abstraction.Operation;
+using AiController.Transmission.SignalR;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AiController.Operation.Operators.Indirect
+{
+    public class Gpt35ClientOperator : IAsyncOperator<DistributeMessageModel?>
+    {
+        [JsonIgnore] public IAsyncOperator<DistributeMessageModel?> ServerSender { get; set; }
+
+        public Task<DistributeMessageModel?> SendAsync(string ask)
+        {
+            return ServerSender.SendAsync($"{Identifier} \n{ask}");
+        }
+
+        public  string Identifier { get; init; }
+        public  string Description { get; set; }
+    }
+}
