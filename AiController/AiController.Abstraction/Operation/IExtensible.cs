71050db6263109f287a620deb8cc92fa2ead2470
Feast <1353216996@qq.com>
2023-03-31 11:25:57 +0800

    Create IExtensible.cs

---
+++ AiController/AiController.Abstraction/Operation/IExtensible.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiController.Abstraction.Operation
+{
+    public interface IExtensible
+    {
+        void Add(IDescriptor descriptor);
+
+        void Remove(IDescriptor descriptor);
+    }
+}
