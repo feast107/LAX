a7e9ce61c67deb0d5daaa3c8624ab1a4709889bf
Feast <1353216996@qq.com>
2023-03-31 11:11:17 +0800

    Create DescriptionAttribute.cs

---
+++ AiController/AiController.Transmission/DescriptionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiController.Transmission
+{
+    public class DescriptionAttribute : Attribute
+    {
+        public readonly string Description;
+
+        public DescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+}
