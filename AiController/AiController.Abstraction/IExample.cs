5600b959a01133fa7118955df4cba55c1715fdaa
Feast <1353216996@qq.com>
2023-03-31 11:07:04 +0800

    Create IExample.cs

---
+++ AiController/AiController.Abstraction/IExample.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiController.Abstraction
+{
+    public interface IExample
+    {
+        public string Example { get; }
+    }
+}
